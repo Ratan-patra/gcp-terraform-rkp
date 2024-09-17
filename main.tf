3a6775d8b10013c833b28cf61c01af65ddcbac99
Patra <ratan-kumar.patra@lloydsbanking.dev>
2024-09-17 17:46:05 +0530

    removed space

--- main.tf
+++ main.tf
@@ -2,6 +2,5 @@ resource "google_storage_bucket" "rkp-my-bucket" {
   name          = "rkp-githubdemo-bucket"
   location      = "US"
   force_destroy = true
-
   public_access_prevention = "enforced"
 }
